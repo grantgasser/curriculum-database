18c33d07fa0f91bb0878ed81923fc81cd8ed20c5
Jackson O'Donnell <Jackson_Odonnell@baylor.edu>
2019-05-05 16:22:06 -0500

    insert_data 1.1

--- Project/insert_data.py
+++ Project/insert_data.py
@@ -202,7 +202,7 @@ def insert_into_section(new_data, mycursor, mydb):
 ##################################################################################
 def insert_into_sec_grades(new_data, mycursor, mydb):
     sql = """INSERT INTO sec_grades (section_id, A+, A, A-, B+, B, B-, C+, C, C-, D+, D, D-, F, I, W)
-             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s""") 
+             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""" 
 
     vals = (new_data['section_id'], new_data['A+'], new_data['A'], new_data['A-'], new_data['B+'],
             new_data['B'], new_data['B-'], new_data['C+'], new_data['C'], new_data['C-'],
@@ -226,7 +226,7 @@ def insert_into_sec_grades(new_data, mycursor, mydb):
 ##################################################################################
 def insert_into_goal_grades(new_data, mycursor, mydb):
     sql = """INSERT INTO goal_grades (goal_id, A+, A, A-, B+, B, B-, C+, C, C-, D+, D, D-, F, I, W)
-             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s""") 
+             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""" 
 
     vals = (new_data['goal_id'], new_data['A+'], new_data['A'], new_data['A-'], new_data['B+'],
             new_data['B'], new_data['B-'], new_data['C+'], new_data['C'], new_data['C-'],
