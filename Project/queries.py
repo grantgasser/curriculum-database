84ae08fe5e70866a5a19d8fd9534e1be33461759
zeedugar <38016844+zeedugar@users.noreply.github.com>
2019-05-06 11:22:37 -0500

    updated queries file

--- Project/queries.py
+++ Project/queries.py
@@ -14,40 +14,54 @@ def get_curric(curric_name, person_id, mycursor):
     return mycursor.fetchall()
 
 def get_course(course_name, mycursor):
-	sql = """SELECT  courses.course_name,* 
-			 FROM courses, curric_ops, curric_reqs
-			 WHERE courses.course_name = %s
-			 AND courses.course_name = curric_ops.course_name
-			 AND courses.course_name = curric_reqs.course_name
-			 GROUP BY courses.course_name
-			 """
+	sql = """SELECT  * 
+			 FROM courses
+			 WHERE course_name = %s"""
+
+	vals = (course_name)
+
+	req = get_reqs(course_name,mycursor)
+
+	ops = get_ops(course_name,mycursor)
+
+	mycursor.execute(sql,vals)
+	return mycursor.fetchall(req,ops)
+
+
+def get_reqs(course_name,mycursor):
+	sql = """SELECT req_by
+	         FROM curric_reqs
+	         WHERE course_name = %s """
 
 	vals = (course_name)
 
 	mycursor.execute(sql,vals)
 	return mycursor.fetchall()
 
+def get_ops(course_name,mycursor):
+	sql = """SELECT op_for
+	         FROM curric_reqs
+	         WHERE course_name = %s """
 
-def get_section(course_name,curric_name,semester,year1, year2, mycursor):
-
-	sql = """SELECT course_name
-			 FROM curric_reqs NATURAL JOIN curric_ops
-			 WHERE course_name = %s
-			 AND (req_for = %s
-			 OR req_opt = %s)
-			 AND course_name IN (
-			 	SELECT course_name, *
-			 	FROM section RIGHT JOIN sec_grades
-			 	WHERE course_name = section.course_name
-				AND semester = %s 
-				AND year BETWEEN %s AND %s
-				AND sec_grades.section_id = section.section_id )
-			 """
-			 
-	vals = (course_name,curric_name,semester,year1,year2)
+	vals = (course_name)
 
 	mycursor.execute(sql,vals)
 	return mycursor.fetchall()
 
+def get_section(curric_name,course_name,semester,year1, year2, mycursor):
 
+	sql = """SELECT *
+			 FROM   sec_grades NATURAL JOIN section
+			 WHERE  course_name = %s
+			 AND semester = %s
+			 AND year BETWEEN %s AND %s
+			 AND course_name IN (SELECT course_name 
+			 					 FROM curric_reqs NATURAL JOIN curric_ops
+			 					 WHERE op_for = %s OR req_by = %s)
+		 """
 
+	vals = (curric_name,course_name,semester,year1, year2)
+
+	mycursor.execute(sql,vals)
+	return mycursor.fetchall();
+			 
